f84749b636e76adb92f6007c2a1b3d67dc2b4869
madeleine2086 <>
2023-11-03 19:40:52 +0000

    Initial commit, create variables

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+ROOM_WIDTH = 10
+ROOM_HEIGHT = 10
+
+key_position_x = randint(0, GAME_WIDTH)
+key_position_y = randint(0, GAME_HEIGHT)
+
+player_position_x = 0
+player_position_y = 0
