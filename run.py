1902c4b19e342ce5efa705a4f31c10da5877c0ac
madeleine2086 <>
2023-11-04 22:25:20 +0000

    Adding option of playing again or quitting the game

--- run.py
+++ run.py
@@ -6,47 +6,57 @@ from random import randint
 ROOM_WIDTH = 10
 ROOM_HEIGHT = 10
 
-key_position_x = randint(0, ROOM_WIDTH)
-key_position_y = randint(0, ROOM_HEIGHT)
-
-player_position_x = 0
-player_position_y = 0
-key_is_found = False
-steps = 0
 player_name = input("What's your name? ")
-print(f"Hello {player_name}! Your task is to find the key in a dark square room.\nYou start in the bottom left corner.")
+print(f"Hello {player_name}!")
 
-print(key_position_x, key_position_y)
+def play_game():
+    key_position_x = randint(0, ROOM_WIDTH)
+    key_position_y = randint(0, ROOM_HEIGHT)
 
-while not key_is_found:
-    steps += 1
-    print("")
-    print("You can choose to go left [A], right [D], up [W] or down [S]: ")
+    player_position_x = 0
+    player_position_y = 0
+    key_is_found = False
+    steps = 0
+    
+    print("Your task is to find the key in a dark square room.\nYou start in the bottom left corner.")
 
-    player_move = input(f"What's your move? ")
+    print(key_position_x, key_position_y)
 
-    if player_move == "a":
-        player_position_x -= 1
-        if player_position_x < 0:
-            print("You hit the wall!!")
-    elif player_move == "d":
-        player_position_x += 1
-        if player_position_x > ROOM_WIDTH:
-            print("You hit the wall!!")
-    elif player_move == "w":
-        player_position_y += 1
-        if player_position_y > ROOM_HEIGHT:
-            print("You hit the wall!!")
-    elif player_move == "s":
-        player_position_y -= 1
-        if player_position_y < 0:
-            print("You hit the wall!!")
-    else:
-        print("Incorrect move, try again!")
+    while not key_is_found:
+        steps += 1
+        print("")
+        print("You can choose to go left [A], right [D], up [W] or down [S]: ")
 
-    if player_position_x == key_position_x and player_position_y == key_position_y:
-        print(f"Congratulations {player_name}! You found the key!")
-        print(f"You needed {steps} moves to find it.")
-        play_again = input("Do you want to play again?\nType [yes] or [no] ")
+        player_move = input(f"What's your move? ")
 
-    print(player_position_x, player_position_y)
+        if player_move == "a":
+            player_position_x -= 1
+            if player_position_x < 0:
+                print("You hit the wall!!")
+        elif player_move == "d":
+            player_position_x += 1
+            if player_position_x > ROOM_WIDTH:
+                print("You hit the wall!!")
+        elif player_move == "w":
+            player_position_y += 1
+            if player_position_y > ROOM_HEIGHT:
+                print("You hit the wall!!")
+        elif player_move == "s":
+            player_position_y -= 1
+            if player_position_y < 0:
+                print("You hit the wall!!")
+        else:
+            print("Incorrect move, try again!")
+
+        if player_position_x == key_position_x and player_position_y == key_position_y:
+            print(f"Congratulations {player_name}! You found the key!")
+            print(f"You needed {steps} moves to find it.")
+            play_again = input("Do you want to play again?\nType [yes] or [no] ")
+            if play_again == "yes":
+                play_game()
+            else:
+                quit()    
+                
+        print(player_position_x, player_position_y)
+
+play_game()
