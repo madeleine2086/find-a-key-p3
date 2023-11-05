04539e75eac6f6ee43bd9a999d599bd0c41d7dba
madeleine2086 <>
2023-11-05 03:57:57 +0000

    Correcting errors highlighted in PEP8

--- README.md
+++ README.md
@@ -1,32 +1,49 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
 
-Welcome,
+## Find The Key - Game Description
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **March 14, 2023**
+- Find The Key is simple but enjoyable, command line game.
+- The player's goal is to find the key that's hidden in a dark, square room. The key can be anywhere and its position is randomly set before the player starts.
+- The player is guided by messages describing player's distance from the key. If the distance is deacreasing, a 'Warmer!' message is printed to the terminal. If the distance is increasing, a 'Cold!' message is printed instead.
+- The player's goal is to find the key by navigating themselves with the help of messages printed while taking steps.
+- The game is written in Python and can be accessed in a mock terminal.
 
-## Reminders
 
-- Your code must be placed in the `run.py` file
-- Your dependencies must be placed in the `requirements.txt` file
-- Do not edit any of the other files or your code may not deploy properly
+## How To Play
 
-## Creating the Heroku app
+1. The player starts the game in a square room, sized 10 x 10.
+2. The player's starting position is 0, 0 - on x and y axis respectively.
+3. The player can go in four different directions, using 'W' for up, 'S' for down, 'A' for left and 'D' for right.
+4. If, after making a move, the player sees 'Warmer' message, distance between the key and the player is smaller than it was before making that move - good direction.
+5. If, after making a move, the player sees 'Cold!' message displayed, distance between the key and the player is bigger than it was before making that move - wrong direction.
+6. When the key is found, the player has the option to continue with another round or quitting the game.
 
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
+## Features
 
-1. `heroku/python`
-2. `heroku/nodejs`
+- Random key position generation, using randint from random library.
+- Accepts user input:
+    - The player types their name, which is then used to display personal 'Congratulations' message.
+    - The player navigates in the room by typing which direction they want to go.
+    - The player decides if they want to continue with another round or stop playing.
+- Input Validation:
+    - Message displayed when player is trying to go outsie the grid size - player can't make a move outside the room.
+    - Message 'Incorrect move, try again!' displayed if values other than 'W', 'S', 'A', 'D' are keyed in.
 
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
+## Future Features
 
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
+- Allow players to select the room size.
+- Allow players to start from different corners of the room.
+-
 
-Connect your GitHub repository and deploy as normal.
+## Testing
 
-## Constraints
+- I have manually tested this project by:
+    - Testing in my local terminal and Code Institute Heroku terminal.
+    - Giving invalid inputs by typing values other than control keys; trying to go outside of the grid size.
 
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
+## Deployment
 
----
+- This project was deployed using Code Institute's mock terminal for Heroku
 
-Happy coding!
+## Credits
+
+- Code Institute for the deployment terminal.
--- run.py
+++ run.py
@@ -8,28 +8,33 @@ from random import randint
 ROOM_WIDTH = 10
 ROOM_HEIGHT = 10
 
-# Asking the player for a name to display the welcome message and use it later in a f string
+# Asking for a name to display the welcome message and use in f string later
 player_name = input("What's your name? ")
 print(f"Hello {player_name}!")
 
 # Creating the function for the game
-def play_game():
-    # Setting a random key position using randint
-    key_position_x = randint(0, ROOM_WIDTH)
-    key_position_y = randint(0, ROOM_HEIGHT)
 
-    player_position_x = 0
-    player_position_y = 0
+
+def play_game():
+
+    # Setting a random key position using randint
+    key_x = randint(0, ROOM_WIDTH)
+    key_y = randint(0, ROOM_HEIGHT)
+
+    player_x = 0
+    player_y = 0
     key_is_found = False
     steps = 0
 
-    # Printing rules og the game to the terminal 
-    print("Your task is to find the key in a dark square room.\nYou start in the bottom left corner.\nDepending on how close to the key you are, you will see either 'Warmer' or 'Cold' message.")
- 
-    # Creating 1st variable needed for calculating the distance between the player and the key
+    # Printing rules og the game to the terminal
+    print("Your task is to find the key in a dark square room.")
+    print("You start in the bottom left corner.")
+    print("We will guide you to the key by displaying 'Warmer' or 'Cold'")
+
+    # Creating 1st variable for calculating distance to the key
     # Variable needed for printing 'Warmer' or 'Cold' message
-    distance_before_steps = sqrt((key_position_x - player_position_x) ** 2 + (key_position_y - player_position_y) ** 2)
-    
+    before_step = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
+
     # Steps in the game while the player is looking for the key:
     while not key_is_found:
         steps += 1
@@ -37,46 +42,55 @@ def play_game():
         print("You can choose to go left [A], right [D], up [W] or down [S]: ")
 
         player_move = input(f"What's your move? ")
-        
-        # Making sure that the player doesn't go outside the grid by displaying appropriate message
-        if player_move == "a":
-            player_position_x -= 1
-            if player_position_x < 0:
-                print("You hit the wall!!")
-        elif player_move == "d":
-            player_position_x += 1
-            if player_position_x > ROOM_WIDTH:
-                print("You hit the wall!!")
-        elif player_move == "w":
-            player_position_y += 1
-            if player_position_y > ROOM_HEIGHT:
-                print("You hit the wall!!")
-        elif player_move == "s":
-            player_position_y -= 1
-            if player_position_y < 0:
-                print("You hit the wall!!")
-        else:
-            # Message displayed in case of using keys other than movement keys: [W, S, A, D]
-            print("Incorrect move, try again!")
 
-        # If statement printing appropriate message in case of a player finding the key
+        # Making sure that the player doesn't go outside the grid
+        if player_move == "a":
+            player_x -= 1
+            if player_x < 0:
+                print("You hit the wall!!")
+                player_x = 0
+        elif player_move == "d":
+            player_x += 1
+            if player_x > ROOM_WIDTH:
+                print("You hit the wall!!")
+                player_x = ROOM_WIDTH
+        elif player_move == "w":
+            player_y += 1
+            if player_y > ROOM_HEIGHT:
+                print("You hit the wall!!")
+                player_y = ROOM_HEIGHT
+        elif player_move == "s":
+            player_y -= 1
+            if player_y < 0:
+                print("You hit the wall!!")
+                player_y = 0
+        else:
+            # Message displayed in case of using keys other than movement keys
+            print("Incorrect move, try again!")
+            continue
+
+        # If statement printing a message in case player finds the key
         # Giving the option to continue the game or to quit
-        if player_position_x == key_position_x and player_position_y == key_position_y:
+        if player_x == key_x and player_y == key_y:
             print(f"Congratulations {player_name}! You found the key!")
             print(f"You needed {steps} steps to find it.")
-            play_again = input("Do you want to play again?\nType [yes] or [no] ")
+            print("Do you want to play again?")
+            play_again = input.lower("Type [yes] or [no] ")
             if play_again == "yes":
                 play_game()
             else:
-                quit()    
+                quit()
         # Second variable needed to display 'Cold' or 'Warmer' message
-        distance_after_steps = sqrt((key_position_x - player_position_x) ** 2 + (key_position_y - player_position_y) ** 2)
+        after_step = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
 
-        if distance_before_steps > distance_after_steps:
+        if before_step > after_step:
             print("Warmer!")
         else:
             print("Cold!")
 
-        distance_before_steps = distance_after_steps
+        # After making one step in either direction,
+        # distance_after_steps becomes distance_before_steps for the next step:
+        before_step = after_step
+
 
 play_game()
